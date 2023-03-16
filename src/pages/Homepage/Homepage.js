a05d9dd799474efdd99368594f9b51af316b6936
Quanabcd <37532986+Quanabcd@users.noreply.github.com>
2023-03-16 10:00:47 +0700

    update homepage

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import MenuBar from '@/components/navbar'
+import MenuBar from '../../components/navbar'
+import Iclogo from '../../assets/home-image/IcLogo.svg'
+import BgBanner from '../../assets/home-image/Bg_Banner.png'
+import styles from './HomePage.module.css';
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
-  
+
+  const title = window.document.title;
+
   return (
-    <div className='app letter-layout'>
-      Homepage
-      <MenuBar />
+    <div className={`${styles.homepage_box}`}>
+      <div className={`${styles.homepage_box_logo}`}>
+        <Link to='/' title={title}>
+          <img src={Iclogo} alt={title} />
+        </Link>
+      </div>
+      <div className={`${styles.homepage_box_navbar}`}>
+        <MenuBar />
+      </div>
     </div>
   )
 }
