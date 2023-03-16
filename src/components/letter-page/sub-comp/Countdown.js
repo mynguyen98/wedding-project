2c5630b200d2342deb90729e7880ff1788b16805
Mỹ Nguyễn <mynguyen@everstar.vn>
2023-03-16 15:35:34 +0700

    commit

--- src/components/letter-page/sub-comp/Countdown.js
+++ src/components/letter-page/sub-comp/Countdown.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import dayjs from 'dayjs'
 
 const CountDown = () => {
-  const timeLeft = dayjs('2023-04-05') - dayjs()
+  const currentDate = dayjs().format('YYYY/MM/DD')
+  const timeLeft = dayjs('2023-03-17') - dayjs(currentDate)
   const dayLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+
   return <h1 className='text-second text-6xl'>{dayLeft}</h1>
 }
 export default CountDown
