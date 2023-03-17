e2d7dea74f578612db6570dec248c86e302fffc6
Quanabcd <37532986+Quanabcd@users.noreply.github.com>
2023-03-17 17:55:19 +0700

    commit

--- src/components/chooseTypeBlock.js
+++ src/components/chooseTypeBlock.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { Button } from './button';
 import { BUTTON_STYLES } from '@/commons/Constant.ts';
 import Languages from '@/commons/Languages';
@@ -6,6 +6,19 @@ import itemImage from '@/assets/home-image/item.png'
 
 function ChooseTypeBlock() {
 
+    const renderSection = useCallback((label, title) => {
+        return <div className='slide-item'>
+            <div className='box-image'>
+                <img src={itemImage} title={'item'} />
+            </div>
+            <div className='title'>
+                <span>{label}</span>
+                <h3>{title}</h3>
+            </div>
+        </div>;
+    }, []);
+
+ 
     return (
         <div className='section_wrap_type_ds'>
 
@@ -16,53 +29,19 @@ function ChooseTypeBlock() {
                     </h2>
                 </div>
                 <div className='slide-track scroll-item-horizontal slide-group-item'>
-                    <div className='slide-item'>
-                        <div className='box-image'>
-                            <img src={itemImage} title={'item'} />
-                        </div>
-                        <div className='title'>
-                            <span>Mẫu 35</span>
-                            <h3>Mùa hè kỉ niệm</h3>
-                        </div>
-                    </div>
-                    <div className='slide-item'>
-                        <div className='box-image'>
-                            <img src={itemImage} title={'item'} />
-                        </div>
-                        <div className='title'>
-                            <span>Mẫu 35</span>
-                            <h3>Mùa hè kỉ niệm</h3>
-                        </div>
-                    </div>
-                    <div className='slide-item'>
-                        <div className='box-image'>
-                            <img src={itemImage} title={'item'} />
-                        </div>
-                        <div className='title'>
-                            <span>Mẫu 35</span>
-                            <h3>Mùa hè kỉ niệm</h3>
-                        </div>
-                    </div>
-                    <div className='slide-item'>
-                        <div className='box-image'>
-                            <img src={itemImage} title={'item'} />
-                        </div>
-                        <div className='title'>
-                            <span>Mẫu 35</span>
-                            <h3>Mùa hè kỉ niệm</h3>
-                        </div>
-                    </div>
 
-                    <div className='slide-item'>
-                        <div className='box-image'>
-                            <img src={itemImage} title={'item'} />
-                        </div>
-                        <div className='title'>
-                            <span>Mẫu 35</span>
-                            <h3>Mùa hè kỉ niệm</h3>
-                        </div>
-                    </div>
-                    
+
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+                    {renderSection('Mẫu 35', 'Mùa hè kỉ niệm')}
+
+
                 </div>
                 <div className='bottom-button-click center'>
                     <Button
