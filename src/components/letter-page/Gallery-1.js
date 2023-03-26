f34c942cb340e0d0e6591fff53d0cc8a54cb8bc9
Mỹ Nguyễn <mynguyen@everstar.vn>
2023-03-26 19:52:42 +0700

    gallery-letter footer letter

--- src/components/letter-page/Gallery-1.js
+++ src/components/letter-page/Gallery-1.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
-import TitleSection from './sub-comp/TitleSection'
-import { galleryImage } from '../../utils/gallery-data'
-import heartIcon from '@/assets/svg/letter-heart.svg'
-import heartIconFill from '@/assets/svg/letter-heart-fill.svg'
-import { Carousel } from 'react-responsive-carousel'
+import React, { useState } from "react";
+import TitleSection from "./sub-comp/TitleSection";
+import { galleryImage } from "../../utils/gallery-data";
+import heartIcon from "@/assets/svg/letter-heart.svg";
+import heartIconFill from "@/assets/svg/letter-heart-fill.svg";
+import { Carousel } from "react-responsive-carousel";
 const Gallery = ({ setModalContent, setIsOpen, setIndex }) => {
-  const [selectedItem, setSelectedItem] = useState(0)
+  const [selectedItem, setSelectedItem] = useState(0);
   const randomNumber = (number) => {
-    return Math.floor(Math.random() * number)
-  }
+    return Math.floor(Math.random() * number);
+  };
   return (
-    <div className='py-10 px-3 section-mb layout-mw' id='gallery'>
-      <TitleSection title='ALBUM' />
+    <div className="py-10 px-3 section-mb layout-mw" id="gallery">
+      <TitleSection title="ALBUM" />
       <div>
         <Carousel
           showStatus={false}
@@ -21,44 +21,54 @@ const Gallery = ({ setModalContent, setIsOpen, setIndex }) => {
         >
           {galleryImage.map((image) => {
             return (
-              <div className='gallery-image mb-3'>
-                <img src={image.imageUrl} alt='image gallery' />
+              <div className="gallery-image mb-3 relative">
+                <img src={image.imageUrl} alt="image gallery" />
+                <div className="absolute bottom-8 right-8 w-6 h-6 flex items-center justify-end cursor-pointer">
+                  <span style={{ color: "white" }} className="mr-1">
+                    {randomNumber(16)}
+                  </span>
+                  <img
+                    src={randomNumber(2) === 0 ? heartIcon : heartIconFill}
+                    alt="heart icon"
+                    className="w-6 h-6"
+                  />
+                </div>
               </div>
-            )
+            );
           })}
         </Carousel>
         {/* <div className='gallery-image mb-3'>
           <img src={galleryImage[0].imageUrl} alt='image gallery' />
         </div> */}
-        <ul className=' gallery-container'>
+        <ul className=" gallery-container">
           {galleryImage.map((image, index) => {
-            const heartRandom = Math.floor(Math.random() * 2)
+            const heartRandom = Math.floor(Math.random() * 2);
             return (
               <li
                 key={index}
-                className='gallery-img relative'
+                className="gallery-img relative"
                 onClick={() => setSelectedItem(index)}
               >
-                <div className='img-container'>
-                  <img src={image.imageUrl} alt='image gallery' />
+                <div className="img-container">
+                  <img src={image.imageUrl} alt="image gallery" />
                 </div>
-                <div className='absolute bottom-2 right-2 w-4 h-4 flex items-center justify-end'>
-                  <span style={{ color: 'white' }} className='mr-1'>
+                <div className="absolute bottom-2 right-2 w-6 h-6 flex items-center justify-end cursor-pointer">
+                  <span style={{ color: "white" }} className="mr-1">
                     {randomNumber(16)}
                   </span>
                   <img
                     src={randomNumber(2) === 0 ? heartIcon : heartIconFill}
-                    alt='heart icon'
-                    className='w-4 h-4'
+                    alt="heart icon"
+                    className="w-4 h-4"
                   />
                 </div>
               </li>
-            )
+            );
           })}
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
