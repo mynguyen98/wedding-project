88943a2ae2fd8703ac08958fdd7325336b4104b1
Quanabcd <37532986+Quanabcd@users.noreply.github.com>
2023-03-29 15:41:17 +0700

    Update index.js

--- src/components/imageUpload/index.js
+++ src/components/imageUpload/index.js
@@ -1,109 +1,135 @@
-import React, { useCallback, useState } from "react";
+import React, { forwardRef, useCallback, useImperativeHandle, useMemo, useState } from "react";
 import ImageUploading from "react-images-uploading";
 import CloseIcon from "../icons/CloseIcon";
 import SortableList, { SortableItem } from "react-easy-sort";
 import arrayMove from "array-move-e5";
 import Languages from "@/commons/Languages";
+import Validate from "@/utils/Validate";
 
-export const ImageUpload = ({ icon, maxW, height, desc, maxnumber, allowDrag }) => {
+export const ImageUpload = forwardRef(
+  ({ icon, maxW, height, desc, maxnumber, allowDrag }, ref) => {
+    useImperativeHandle(ref, () => ({
+      setErrorMsg
+    }));
 
-  const [images, setImages] = useState([]);
-  const maxNumber = maxnumber || 10;
+    const [images, setImages] = useState([]);
+    const [errMsg, setErrMsg] = useState("");
 
-  const onChange = (imageList) => {
-    setImages(imageList);
-  };
+    const maxNumber = maxnumber || 10;
 
-  const onError = () => {
-    console.log('Số lượng tối đa ' + maxnumber + ' ảnh')
-  };
+    const onChange = (imageList) => {
+      setImages(imageList);
+    };
 
-  const onSortEnd = useCallback((oldIndex, newIndex) => {
-    setImages((array) => arrayMove(array, oldIndex, newIndex));
-    console.log(images)
-  }, [images]);
+    const onError = () => {
+      console.log('Số lượng tối đa ' + maxnumber + ' ảnh')
+    };
 
-  return (
-    <div className="wrap_box_upload_image_section">
-      <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-        acceptType={["jpg", "png", "jpeg", "HEIC"]}
-        onError={onError}
-      >
-        {({
+    const onSortEnd = useCallback((oldIndex, newIndex) => {
+      setImages((array) => arrayMove(array, oldIndex, newIndex));
+      console.log(images)
+    }, [images]);
 
-          imageList,
-          onImageUpload,
-          onImageUpdate,
-          onImageRemove,
-          dragProps,
+    const setErrorMsg = useCallback((msg) => {
+      if (Validate.isEmptyValue(msg)) {
+        return;
+      }
+      setErrMsg(msg);
+    }, []);
 
-        }) => (
-          // write your building UI
-          <div className="wrap_box_upload_image_child">
-            <SortableList
-              onSortEnd={onSortEnd}
-              className={'root-remove'}
-              draggedItemClassName={'dragged'}
-              defaultChecked
-              draggable
-              hidden
-              allowDrag={allowDrag || false}
-            >
-              {
-                imageList.map((image, index) =>
-                  <SortableItem key={index} imgProps={{ draggable: false }}>
-                    <div className="image-item flex justify-center">
-                      <div
-                        className="relative max-w-fit"
-                        style={{ height: height }}
-                        {...dragProps}
-                      >
+    const errorMessage = useMemo(() => {
+      if (!Validate.isEmptyValue(errMsg)) {
+        return (
+          <div className='messageError'>
+            <p>{errMsg}</p>
+          </div>
+        );
+      }
+      return null;
+    }, [errMsg]);
+
+    return (
+      <div className="wrap_box_upload_image_section">
+        <ImageUploading
+          multiple
+          value={images}
+          onChange={onChange}
+          maxNumber={maxNumber}
+          dataURLKey="data_url"
+          acceptType={["jpg", "png", "jpeg","bmp", ".gif", "HEIC"]}
+          onError={onError}
+        >
+          {({
+
+            imageList,
+            onImageUpload,
+            onImageUpdate,
+            onImageRemove,
+            dragProps,
+
+          }) => (
+            // write your building UI
+            <div className="wrap_box_upload_image_child">
+              <SortableList
+                onSortEnd={onSortEnd}
+                className={'root-remove'}
+                draggedItemClassName={'dragged'}
+                defaultChecked
+                draggable
+                hidden
+                allowDrag={allowDrag || false}
+              >
+                {
+                  imageList.map((image, index) =>
+                    <SortableItem key={index} imgProps={{ draggable: false }}>
+                      <div className="image-item flex justify-center">
                         <div
-                          className="absolute pointer"
-                          onClick={() => onImageRemove(index)}
+                          className="relative max-w-fit"
+                          style={{ height: height }}
+                          {...dragProps}
                         >
-                          <CloseIcon />
+                          <div
+                            className="absolute pointer"
+                            onClick={() => onImageRemove(index)}
+                          >
+                            <CloseIcon />
+                          </div>
+                          <img
+                            src={image.data_url}
+                            alt={'thumbs' + image.file?.size}
+                            style={{ maxHeight: "100%" }}
+                            onClick={() => onImageUpdate(index)}
+                          />
                         </div>
-                        <img
-                          src={image.data_url}
-                          alt={'thumbs' + image.file?.size}
-                          style={{ maxHeight: "100%" }}
-                          onClick={() => onImageUpdate(index)}
-                        />
+                      </div>
+                    </SortableItem>
+                  )
+                }
+                {
+                  images.length < maxNumber && <div
+                    className="wrap_imageUploading border-img-dash flex items-center"
+                    style={{ maxWidth: maxW, height: height }}
+                    onClick={onImageUpload}
+                    {...dragProps}
+                  >
+                    <div className="justify-center">
+                      <div className='ImgUploadIcon'>
+                        {icon}
                       </div>
                     </div>
-                  </SortableItem>
-                )
-              }
-              {
-                images.length < maxNumber && <div
-                  className="wrap_imageUploading border-img-dash flex items-center"
-                  style={{ maxWidth: maxW, height: height }}
-                  onClick={onImageUpload}
-                  {...dragProps}
-                >
-                  <div className="justify-center">
-                    <div className='ImgUploadIcon'>
-                      {icon}
+                    <div className="text-center">
+                      <p className="add_image_uploading">
+                        {Languages.text.addonepic}
+                      </p>
+                      {desc && <p className="desc_image_uploading">{desc}</p>}
                     </div>
+                    {errorMessage}
                   </div>
-                  <div className="text-center">
-                    <p className="add_image_uploading">
-                      {Languages.text.addonepic}
-                    </p>
-                    {desc && <p className="desc_image_uploading">{desc}</p>}
-                  </div>
-                </div>
-              }
-            </SortableList>
-          </div>
-        )}
-      </ImageUploading>
-    </div>
-  );
-};
+                }
+              </SortableList>
+            </div>
+          )}
+        </ImageUploading>
+      </div>
+    );
+  });
