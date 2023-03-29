646aee55f0565ca937426db62cd64849f377a87f
Quanabcd <37532986+Quanabcd@users.noreply.github.com>
2023-03-29 15:20:21 +0700

    update

--- src/components/icons/CloseIcon.js
+++ src/components/icons/CloseIcon.js
@@ -9,18 +9,18 @@ const CloseIcon = () => {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <rect width='24' height='24' rx='4' fill='white' fillOpacity='0.25' />
+      <rect width='24' height='24' rx='4' fill='white' fillOpacity='0' />
       <path
         d='M17.25 17.25L6.75 6.75'
         stroke='white'
-        strokeWidth='1.5'
+        strokeWidth='2'
         strokeLinecap='round'
         strokeLinejoin='round'
       />
       <path
         d='M17.25 6.75L6.75 17.25'
         stroke='white'
-        strokeWidth='1.5'
+        strokeWidth='2'
         strokeLinecap='round'
         strokeLinejoin='round'
       />
