fd77ffce99d0b59838beda4c02fb27c98092c073
Quanabcd <37532986+Quanabcd@users.noreply.github.com>
2023-04-04 17:30:45 +0700

    update createpage

---
+++ src/commons/FieldsDataObj.js
@@ -0,0 +1,66 @@
+export const SelectInvitationTemplate = [
+    {
+        id: 'invitation_one',
+        name: 'invitation_one',
+        value: 'invitation_one',
+        text: 'Trời ban phận đẹp duyên ưa Ngày hay tháng tốt se tơ sợi hồng Trăm năm một kiếp mặn nồng Xin mời quan khách đến cùng chung vui'
+    },
+    {
+        id: 'invitation_two',
+        name: 'invitation_two',
+        value: 'invitation_two',
+        text: 'Người thương của <TÊN CÔ DÂU> <>TÊN CHÚ RỂ> ơi, cảm ơn vì đã có mặt trong lễ cưới tuyệt vời này nhé!'
+    },
+    ,
+    {
+        id: 'invitation_three',
+        name: 'invitation_three',
+        value: 'invitation_three',
+        text: 'Sự hiện diện của bạn là minh chứng cho tình yêu của chúng tôi...'
+    }
+];
+
+export const SelectTimeTemplate = [
+    {
+        id: 'timetemplate_one',
+        name: 'timetemplate_one',
+        value: 'timetemplate_one',
+        text: 'Người thương của <TÊN CÔ DÂU> <>TÊN CHÚ RỂ> ơi, cảm ơn vì đã có mặt trong lễ cưới tuyệt vời này nhé!'
+    }
+];
+
+export const SelectWarningTemplate = [
+    {
+        id: 'warning_one',
+        name: 'warning_one',
+        value: 'warning_one',
+        text: 'Đừng quên mang theo chiếc vé thông hành trên tay bạn tới bữa tiệc nha!'
+    },
+    {
+        id: 'warning_two',
+        name: 'warning_two',
+        value: 'warning_two',
+        text: 'Dresscode'
+    }
+];
+
+export const SelectSavePenTemplate = [
+    {
+        id: 'savepen_one',
+        name: 'savepen_one',
+        value: 'savepen_one',
+        text: 'Đừng quên để lại lời chúc ngọt ngào của bạn cho khởi đầu trăm năm của cặp dâu rể mới cưới này tại đây nha!'
+    },
+    {
+        id: 'savepen_two',
+        name: 'savepen_two',
+        value: 'savepen_two',
+        text: 'Còn cách nào tuyệt vời hơn việc để lại lời chúc phúc và kỷ niệm đẹp đẽ mà bạn đã có trong lễ thành hôn này với một chữ ký thật xinh nhỉ?'
+    },
+    {
+        id: 'savepen_three',
+        name: 'savepen_three',
+        value: 'savepen_three',
+        text: 'Hãy cùng chúng tôi xây đắp những kỷ niệm đẹp bằng cách để lại lời chúc phúc và chữ ký của bạn trong cuốn sổ này nhé!'
+    }
+];
